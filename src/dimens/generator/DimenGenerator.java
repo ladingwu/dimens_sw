17681efdaa76849809145fe8b2fe479e7adff52e
ladingwu <851547570@qq.com>
2018-08-27 12:19:47 +0800

    update: 更新最设计稿的最小宽度的判断

--- src/dimens/generator/DimenGenerator.java
+++ src/dimens/generator/DimenGenerator.java
@@ -6,20 +6,20 @@ import dimens.utils.MakeUtils;
 public class DimenGenerator {
 
     /**
-     * 设计稿尺寸(根据自己设计师的设计稿的宽度填入)
+     * 设计稿尺寸(将自己设计师的设计稿的宽度填入)
      */
     private static final int DESIGN_WIDTH = 375;
 
     /**
-     * 设计稿高度  没用到
+     * 设计稿的高度  （将自己设计师的设计稿的高度填入）
      */
     private static final int DESIGN_HEIGHT = 667;
 
     public static void main(String[] args) {
-
+        int smallest = DESIGN_WIDTH>DESIGN_HEIGHT? DESIGN_HEIGHT:DESIGN_WIDTH;  //     求得最小宽度
         DimenTypes[] values = DimenTypes.values();
         for (DimenTypes value : values) {
-            MakeUtils.makeAll(DESIGN_WIDTH, value, "/androidui/adapter");
+            MakeUtils.makeAll(smallest, value, "/androidui/adapter");
         }
 
     }
